f8e793f245c41a6f6dbde791822f204a0d230032
Luis Felipe Miléo <mileo@kmee.com.br>
2024-12-04 17:30:30 -0300

    [FIX][hr_holidays_natural_period] date min/max start/end

--- hr_holidays_natural_period/models/hr_leave.py
+++ hr_holidays_natural_period/models/hr_leave.py
@@ -1,6 +1,8 @@
 # Copyright 2020-2024 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from datetime import datetime
+
 from odoo import api, models
 
 
@@ -8,9 +10,12 @@ class HrLeave(models.Model):
     _inherit = "hr.leave"
 
     def _get_number_of_days(self, date_from, date_to, employee_id):
-        instance = self.with_context(
-            natural_period=bool(self.holiday_status_id.request_unit == "natural_day")
-        )
+        instance = self
+        if self.holiday_status_id.request_unit:
+            instance = self.with_context(natural_period=True)
+            date_from = datetime.combine(date_from, datetime.min.time())
+            date_to = datetime.combine(date_to, datetime.max.time())
+
         return super(HrLeave, instance)._get_number_of_days(
             date_from, date_to, employee_id
         )
